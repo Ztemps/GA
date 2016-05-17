050d9c9c0896fb0f54a27fd0f4238b126ce4cf36
xmurcia <murciaxavi@gmail.com>
2016-05-17 17:39:23 +0200

    extend to download xls file

--- src/com/example/view/AdminView/Reports/AdminViewReportsJava.java
+++ src/com/example/view/AdminView/Reports/AdminViewReportsJava.java
@@ -1,8 +1,11 @@
 package com.example.view.AdminView.Reports;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.sql.SQLException;
 import java.text.ParseException;
@@ -13,7 +16,10 @@ import com.example.Templates.MainContentView;
 import com.example.view.AdminView.AdminView;
 import com.example.view.AdminView.CSV.AdminViewCSVUploadJava.FileReciverStudents;
 import com.example.view.AdminView.CSV.AdminViewCSVUploadJava.FileReciverTeachers;
+import com.vaadin.server.FileDownloader;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.server.Page;
+import com.vaadin.server.StreamResource;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Image;
@@ -24,10 +30,15 @@ import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Upload.Receiver;
 import com.vaadin.ui.Upload.SucceededEvent;
 import com.vaadin.ui.Upload.SucceededListener;
+import com.vaadin.ui.themes.ValoTheme;
 
 public class AdminViewReportsJava extends MainContentView {
 	private File file;
 	private AdminReportCSVUpload csv;
+	Button generateReportTrimestre1;
+	StreamResource sr = getXLS();
+	FileDownloader fileDownloader = new FileDownloader(sr);
+	
 	/*private FileReciverTrimestre2 receiver2 = new FileReciverTrimestre2();
 	private FileReciverTrimestre3 receiver3 = new FileReciverTrimestre3();
 	private FileReciverTotal receiver4 = new FileReciverTotal();*/
@@ -35,27 +46,27 @@ public class AdminViewReportsJava extends MainContentView {
 	public AdminViewReportsJava(){
 		csv = new AdminReportCSVUpload();
 		buttonsSettings();
-
+		
 		// INFORMES TRIMESTRALS
 		
+		csv.mainTrimestral.addStyleName("whiteBackground");
+		csv.txtUpTrimestral.addStyleName("settings");
 		csv.txtUpTrimestral.setValue("Carrega d'Informes Trimestrals");
-
 		csv.hTrimestral.addStyleName("csvstudent");
 		csv.hTrimestral.removeAllComponents();
-
+		csv.hTrimestral.addComponent(generateReportTrimestre1);
 	//	csv.horizontalTrimestral.setComponentAlignment(uploadStudent, Alignment.MIDDLE_CENTER);
 		
 		
 		//INFORMES ANUALS
 
 		csv.txtUpTotal.setValue("Carrega d'Informes Anuals");
-
 		csv.hTotal.addStyleName("csvstudent");
 	//	csv.hTotal.removeAllComponents();
 	//	csv.hTotal.addComponents(uploadtrimestre1);
 
 	//	csv.horizontalTotal.setComponentAlignment(uploadStudent, Alignment.MIDDLE_CENTER);
-		
+		fileDownloader.extend(generateReportTrimestre1);
 		
 	}
 	
@@ -63,9 +74,53 @@ public class AdminViewReportsJava extends MainContentView {
 	
 	
 
+	private StreamResource getXLS() {
+		// TODO Auto-generated method stub
+	        StreamResource.StreamSource source = new StreamResource.StreamSource() {
+
+	            public InputStream getStream() {
+	                // return your file/bytearray as an InputStream
+	            	 File excel = new File("/home/xmurcia/Escritorio/horesBUS.ods");
+	            	 InputStream targetStream = null;
+					try {
+						targetStream = new FileInputStream(excel);
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	                
+					return targetStream;
+
+	            }
+	        };
+	      StreamResource resource = new StreamResource ( source, getFileName());
+	        return resource;
+	}
+	
+
+
+
+
+
+	private String getFileName() {
+		// TODO Auto-generated method stub
+		
+		String suggestedSaveFile = "Informe1erTrimestre.xls";
+		
+		return suggestedSaveFile;
+	}
+
+
+
+
+
 	private void buttonsSettings() {
 		// TODO Auto-generated method stub
-	//	vHorizontalMain.addComponent(csv);
+		generateReportTrimestre1 = new Button("Genera informe 1r trimestre", FontAwesome.CLOUD_DOWNLOAD);
+		generateReportTrimestre1.setStyleName(ValoTheme.BUTTON_PRIMARY);
+		generateReportTrimestre1.addStyleName("settings");
+		
+		vHorizontalMain.addComponent(csv);
 		bAdd.setVisible(false);
 		buttonEdit.setVisible(false);
 		bDelete.setVisible(false);
@@ -80,6 +135,8 @@ public class AdminViewReportsJava extends MainContentView {
 		// AdminViewCarregarCSVJava upload = new AdminViewCarregarCSVJava();
 
 	}
+	
+	
 	
 	
 	
