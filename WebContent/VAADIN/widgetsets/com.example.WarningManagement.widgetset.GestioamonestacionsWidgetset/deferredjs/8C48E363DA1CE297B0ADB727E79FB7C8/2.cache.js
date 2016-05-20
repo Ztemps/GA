8c0b1ff5b8ea50b2013f7b7f836f6f7c00afef16
Gerard Paulino <gpaulino@elpuig.xeill.net>
2016-05-20 17:11:10 +0200

    Java doc line lincense implemented

--- WebContent/VAADIN/widgetsets/com.example.WarningManagement.widgetset.GestioamonestacionsWidgetset/deferredjs/8C48E363DA1CE297B0ADB727E79FB7C8/2.cache.js
+++ WebContent/VAADIN/widgetsets/com.example.WarningManagement.widgetset.GestioamonestacionsWidgetset/deferredjs/8C48E363DA1CE297B0ADB727E79FB7C8/2.cache.js
@@ -1,1 +1,12 @@
+/*******************************************************************************
+ * Gestió d'Amonestacions v1.0
+ *
+ * Esta obra está sujeta a la licencia Reconocimiento-NoComercial-SinObraDerivada 4.0 Internacional de Creative Commons. 
+ * Para ver una copia de esta licencia, visite http://creativecommons.org/licenses/by-nc-nd/4.0/.
+ *  
+ * @author Francisco Javier Casado Moreno - fcasasdo@elpuig.xeill.net 
+ * @author Daniel Pérez Palacino - dperez@elpuig.xeill.net 
+ * @author Gerard Enrique Paulino Decena - gpaulino@elpuig.xeill.net 
+ * @author Xavier Murcia Gámez - xmurica@elpuig.xeill.net 
+ *******************************************************************************/
 $wnd.com_example_WarningManagement_widgetset_GestioamonestacionsWidgetset.runAsyncCallback2('slb(1652,1,A8d);_.ce=function npc(){_8b((!U8b&&(U8b=new f9b),U8b),this.a.d)};b1d(An)(2);\n//# sourceURL=com.example.WarningManagement.widgetset.GestioamonestacionsWidgetset-2.js\n')
--- src/com/example/view/AdminView/Group/AdminViewGroupJava.java
+++ src/com/example/view/AdminView/Group/AdminViewGroupJava.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Gestió d'Amonestacions v1.0
+ *
+ * Esta obra está sujeta a la licencia Reconocimiento-NoComercial-SinObraDerivada 4.0 Internacional de Creative Commons. 
+ * Para ver una copia de esta licencia, visite http://creativecommons.org/licenses/by-nc-nd/4.0/.
+ *  
+ * @author Francisco Javier Casado Moreno - fcasasdo@elpuig.xeill.net 
+ * @author Daniel Pérez Palacino - dperez@elpuig.xeill.net 
+ * @author Gerard Enrique Paulino Decena - gpaulino@elpuig.xeill.net 
+ * @author Xavier Murcia Gámez - xmurica@elpuig.xeill.net 
+ *******************************************************************************/
 package com.example.view.AdminView.Group;
 
 import java.util.Date;
