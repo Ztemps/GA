a7fe0450e83177d7a9b6504aa15a96903f0de02f
xmurcia <murciaxavi@gmail.com>
2016-05-25 14:05:22 +0200

    admin pass ok

--- src/com/example/view/AdminView/Settings/AdminViewSettingsJava.java
+++ src/com/example/view/AdminView/Settings/AdminViewSettingsJava.java
@@ -20,13 +20,24 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ResourceBundle;
 import java.util.StringTokenizer;
 
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
+
 import com.example.Dates.ConverterDates;
+import com.example.Entities.User;
+import com.example.Logic.UserJPAManager;
 import com.example.Templates.MainContentView;
 import com.vaadin.data.Property.ReadOnlyException;
 import com.vaadin.data.util.converter.Converter.ConversionException;
@@ -44,10 +55,12 @@ public class AdminViewSettingsJava extends MainContentView {
 	private AdminViewSettings adminsettings;
 	private ConverterDates dates;
 	private ResourceBundle rb = ResourceBundle.getBundle("GA");
+	private User usuari;
+	UserJPAManager MA;
 
 	public AdminViewSettingsJava() {
 		adminsettings = new AdminViewSettings();
-
+		NewPassTextFieldProperties();
 		GeneralSettings();
 		listeners();
 
@@ -56,6 +69,98 @@ public class AdminViewSettingsJava extends MainContentView {
 		} catch (ReadOnlyException | ConversionException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+		
+		adminsettings.checkOldPass.addClickListener(new ClickListener() {
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+				// TODO Auto-generated method stub
+				try {
+					CheckPassword();
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});
+		
+		adminsettings.enterNewPass.addClickListener(new ClickListener() {
+
+			@Override
+			public void buttonClick(ClickEvent event) {
+				// TODO Auto-generated method stub
+				MA = new UserJPAManager();
+				MessageDigest md = null;
+				try {
+					md = MessageDigest.getInstance("SHA-1");
+				} catch (NoSuchAlgorithmException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				HexBinaryAdapter hbinary = new HexBinaryAdapter();
+				String password = adminsettings.newPass.getValue();
+				String confirmpass = adminsettings.confirmNewPass.getValue();
+				
+				if(password.equals(confirmpass)){
+					String passwordhash = hbinary.marshal(md.digest(password.getBytes())).toLowerCase();
+					System.out.println("Encriptada: " + passwordhash);
+
+					int id = Integer.parseInt(getUI().getSession().getAttribute("id").toString());
+
+					
+					MA.updateUser(new User(id, passwordhash));
+					MA.closeTransaction();
+					notif("Contraseña nova acceptada. ");
+					ShowNewPassTextField();
+					ClearFields();
+				}
+				else{
+					notifWrong("La contrasenya nova no coincideix als dos camps. Torni a intentar-ho.");
+					ShowNewPassTextField();
+					ClearFields();
+				}
+				
+
+			}
+		});
+	}
+	
+	
+	private void NewPassTextFieldProperties() {
+		
+		adminsettings.changePassLayout.setCaption("Canvi de password. Introdueixi password actual. ");
+		adminsettings.newPass.setVisible(false);	
+		adminsettings.confirmNewPass.setVisible(false);
+		adminsettings.enterNewPass.setVisible(false);
+	}
+	
+	public void ClearFields() {
+		adminsettings.oldPass.clear();
+		adminsettings.newPass.clear();
+		adminsettings.confirmNewPass.clear();
+	}
+	
+	private void ShowNewPassTextField() {
+
+		if (adminsettings.oldPass.isVisible()) {
+
+			adminsettings.oldPass.setVisible(false);
+			adminsettings.checkOldPass.setVisible(false);
+			adminsettings.changePassLayout.setCaption("Introdueixi password nou als dos camps. ");
+			adminsettings.newPass.setVisible(true);
+			adminsettings.confirmNewPass.setVisible(true);
+			adminsettings.enterNewPass.setVisible(true);
+		} else {
+			adminsettings.oldPass.setVisible(true);
+			adminsettings.checkOldPass.setVisible(true);
+			adminsettings.newPass.setVisible(false);
+			adminsettings.confirmNewPass.setVisible(false);
+			adminsettings.enterNewPass.setVisible(false);
 		}
 	}
 
@@ -69,11 +174,19 @@ public class AdminViewSettingsJava extends MainContentView {
 		adminsettings.checkEmailPares.addStyleName("settings");
 		adminsettings.checkWhatsPares.addStyleName("settings");
 		adminsettings.startDateGrade.addStyleName("settings");
-		adminsettings.endGradeDate.addStyleName("settings");
+		adminsettings.endGradeDate3.addStyleName("settings");
 		adminsettings.firstTrimesterEndDate.addStyleName("settings");
 		adminsettings.secondTrimesterEndDate.addStyleName("settings");
 		adminsettings.secondTrimesterStartDate.addStyleName("settings");
-		adminsettings.thirdTrimesterStartDate.addStyleName("settings");
+		adminsettings.thirdTrimesterStartDate3.addStyleName("settings");
+		
+	
+
+		/*adminsettings.oldPass.setCaption("Introdueixi contrasenya actual per canviar. ");
+		adminsettings.newPass.setCaption("Introdueixi nova contrasenya. ");
+		adminsettings.confirmNewPass.setCaption("Repeteixi nova contrasenya");*/
+		
+		
 		
 		adminsettings.checkEmailTutors.setVisible(false);
 		
@@ -133,13 +246,13 @@ public class AdminViewSettingsJava extends MainContentView {
 
 			Date datainicitrimestre1 = adminsettings.startDateGrade.getValue();
 			datainicitrimestre1buena = dates.converterDate(datainicitrimestre1);
-			Date datafinaltrimestre3 = adminsettings.endGradeDate.getValue();
+			Date datafinaltrimestre3 = adminsettings.endGradeDate3.getValue();
 			datafinaltrimestre3buena = dates.converterDate(datafinaltrimestre3);
 			Date datafintrimestre1 = adminsettings.firstTrimesterEndDate.getValue();
 			datafintrimestre1buena = dates.converterDate(datafintrimestre1);
 			Date datafintrimestre2 = adminsettings.secondTrimesterEndDate.getValue();
 			datafintrimestre2buena = dates.converterDate(datafintrimestre2);
-			Date fechaIniciTrimestre3 = adminsettings.thirdTrimesterStartDate.getValue();
+			Date fechaIniciTrimestre3 = adminsettings.thirdTrimesterStartDate3.getValue();
 			fechaIniciTrimestre3buena = dates.converterDate(fechaIniciTrimestre3);
 			Date fechaIniciTrimestre2 = adminsettings.secondTrimesterStartDate.getValue();
 			fechaIniciTrimestre2buena = dates.converterDate(fechaIniciTrimestre2);
@@ -251,16 +364,81 @@ public class AdminViewSettingsJava extends MainContentView {
 			}
 
 			adminsettings.startDateGrade.setValue(fechaIniciTrimestre1);
-			adminsettings.endGradeDate.setValue(fechaFinalTrimestre3);
+			adminsettings.endGradeDate3.setValue(fechaFinalTrimestre3);
 			adminsettings.firstTrimesterEndDate.setValue(fechafinaltrimestre1);
 			adminsettings.secondTrimesterEndDate.setValue(fechafinaltrimestre2);
 			adminsettings.checkEmailTutors.setValue(checkTutor);	
 			adminsettings.checkEmailPares.setValue(checkPares);
 			adminsettings.checkWhatsPares.setValue(checkTelegram);
 			adminsettings.secondTrimesterStartDate.setValue(fechaIniciTrimestre2);
-			adminsettings.thirdTrimesterStartDate.setValue(fechaIniciTrimestre3);
+			adminsettings.thirdTrimesterStartDate3.setValue(fechaIniciTrimestre3);
 
 		}
+	}
+	
+	private void CheckPassword() throws ClassNotFoundException, SQLException {
+
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("SHA-1");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		HexBinaryAdapter hbinary = new HexBinaryAdapter();
+
+		String dbURL = "jdbc:postgresql:GAdb";
+		Class.forName("org.postgresql.Driver");
+		Connection conn = null;
+		conn = DriverManager.getConnection(dbURL, "postgres", "postgres");
+
+		Statement st = conn.createStatement();
+
+		ResultSet rs = st.executeQuery("SELECT id_docent,contrasenya,usuari,rol FROM usuari");
+
+		String password = adminsettings.oldPass.getValue();
+		String passwordhash = hbinary.marshal(md.digest(password.getBytes())).toLowerCase();
+		System.out.println("NOMEOLVIDES : " + passwordhash);
+
+		while (rs.next()) {
+			usuari = new User();
+			usuari.setId(Integer.parseInt(rs.getString(1)));
+			usuari.setPassword(rs.getString(2));
+			usuari.setUsername(rs.getString(3));
+			usuari.setRol(rs.getString(4));
+
+			if (usuari.getUsername().equals(getUI().getSession().getAttribute("user"))
+					&& usuari.getPassword().equals(passwordhash)) {
+				ShowNewPassTextField();
+				break;
+
+				// Si lo comprueba y es correcto se ocultan estos textfield y
+				// aparece el de nueva contraseña
+			}
+
+		}
+
+		if (!(usuari.getUsername().equals(getUI().getSession().getAttribute("user"))
+				&& usuari.getPassword().equals(passwordhash))) {
+
+			notif("Contraseña no existeix");
+		}
+
+		rs.close();
+		st.close();
+		conn.close();
+
+	}
+	
+	public void notifWrong(String mensaje) {
+
+		Notification notif = new Notification(mensaje, null, Notification.Type.WARNING_MESSAGE, true); // Contains
+																												// HTML
+
+		// Customize it
+		notif.show(Page.getCurrent());
+		notif.setDelayMsec(500);
+		notif.setPosition(Position.TOP_CENTER);
 	}
 
 }
